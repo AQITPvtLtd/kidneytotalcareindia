40a82933406548adb22c764a5eddfc15f6c40ae5
Palak <98663148+palak-7@users.noreply.github.com>
2024-08-30 16:59:30 +0530

    sitemap error temperory solved

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,14 +1,14 @@
-import { getBlog } from "@/services/getBlog";
+// import { getBlog } from "@/services/getBlog";
 
 export default async function sitemap() {
   const baseUrl = process.env.BASE_URL;
-  const response = await getBlog();
-  const blogPosts = response.result?.map((post) => {
-    return {
-      url: `${baseUrl}/blog/${post?.url}`,
-      lastModified: post?.date,
-    };
-  });
+  // const response = await getBlog();
+  // const blogPosts = response.result?.map((post) => {
+  //   return {
+  //     url: `${baseUrl}/blog/${post?.url}`,
+  //     lastModified: post?.date,
+  //   };
+  // });
   return [
     {
       url: baseUrl,
