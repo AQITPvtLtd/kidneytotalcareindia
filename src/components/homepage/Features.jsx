ed8e4b3a7d0c22cea262a0592f7de2b820f44f1d
Palak <98663148+palak-7@users.noreply.github.com>
2024-09-12 13:04:35 +0530

    fixed features section

--- src/components/homepage/Features.jsx
+++ src/components/homepage/Features.jsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const Features = () => {
   return (
-    <div className="relative">
+    <div className="lg:relative">
       <div>
         <Image
           src="/features/features.png"
           width={1000}
           height={1000}
-          className="lg:h-[500px] h-[1000px] w-full object-cover"
+          className="lg:block hidden h-[500px] w-full object-cover"
           alt="feature"
         />
       </div>
-      <div className="lg:flex items-stretch absolute inset-0 lg:p-10 p-5 gap-10">
+      <div className="lg:flex items-stretch lg:absolute lg:inset-0 lg:p-10 p-5 gap-10">
         {/* Expertise and Experience */}
-        <div className="bg-white lg:m-10 lg:p-7 p-4 rounded-xl flex-1">
+        <div className="lg:bg-white lg:m-10 shadow-md bg-sky-100 lg:p-7 p-4 rounded-xl flex-1">
           <div className="font-bold text-2xl pb-3">
             Expertise and Experience
           </div>
@@ -30,7 +30,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="bg-white lg:m-10 mt-4 p-4 lg:p-7 rounded-xl flex-1">
+        <div className="lg:bg-white shadow-md bg-sky-100 lg:m-10 mt-4 p-4 lg:p-7 rounded-xl flex-1">
           <div className="font-bold text-2xl pb-3">
             The Importance of Choosing the Right Nephrologist in Delhi
           </div>
